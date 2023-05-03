08754e66e6365cfc58a62dd238ac049f5719ea9c
Rohan Kurella <83125268+rohankurella02@users.noreply.github.com>
2023-05-03 09:23:07 +0530

    Update user.js

--- API/user.js
+++ API/user.js
@@ -3,7 +3,7 @@ const Userapp=exp.Router()
 Userapp.use(exp.json())
 const expressAsyncHandler=require('express-async-handler')
 const jwt=require('jsonwebtoken')
-const bcryptjs=require('bcryptjs')
+// const bcryptjs=require('bcryptjs')
 
 // Cloudinary
 // var cloudinary = require("cloudinary").v2;
@@ -55,7 +55,7 @@ Userapp.post('/create-users',expressAsyncHandler(async(request,response)=>{
     else{
         
 
-        let hashedPassword= await bcryptjs.hash(newUser.Password,5);
+        let hashedPassword= newUser.Password
         newUser.Password=hashedPassword;
         let result =await userCollectionObject.insertOne(newUser);
         response.send({message:'User has created Successfully.Now please Login'})
@@ -68,13 +68,15 @@ Userapp.post('/login',expressAsyncHandler(async(request,response)=>{
     let newUser=request.body
     let userCollectionObject=request.app.get('userCollectionObject');
     let resultOfUserName= await userCollectionObject.findOne({UserName:newUser.UserName});
+  let result = false
     if(resultOfUserName!=null){
-        let result= await bcryptjs.compare(newUser.Password,resultOfUserName.Password);
+      if(newUser.Password ==resultOfUserName.Password) {
+        result = true}
         if(result==false){
             response.send({message:"Invalid password"})
         }
         else{
-            let token=jwt.sign({UserName:resultOfUserName.UserName},process.env.SECRET_KEY,{expiresIn:1000})
+            let token=jwt.sign({UserName:resultOfUserName.UserName},'abcdef',{expiresIn:1000})
             response.send({message:'login is successful',payload:token,userObj:resultOfUserName});
         }
 
@@ -85,49 +87,7 @@ Userapp.post('/login',expressAsyncHandler(async(request,response)=>{
 
 }))
 
-Userapp.put('/ForgetPassword',expressAsyncHandler(async(request,response)=>{
-    let updateBody=request.body
-    let userName=updateBody.Name
-    let userCollectionObject=request.app.get('userCollectionObject');
-    let resultOfUserName= await userCollectionObject.findOne({Name: userName});
-    
-    if(resultOfUserName!=null){
-        let Newpassword=updateBody.Password;
-        let hashedPassword=await bcryptjs.hash(Newpassword,5);
-        let result= await userCollectionObject.updateOne({Name:userName},{$set:{Password:hashedPassword}});
-        response.send({message:'Password Changed'})
 
-    }
-    else{
-        response.send({message:'User not found. Please Signup'})
-    }
-        
-    
-}))
-
-
-Userapp.put('/ChangePassword',expressAsyncHandler(async(request,response)=>{
-    let userBody=request.body
-    let userCollectionObject=request.app.get('userCollectionObject');
-    let resultOfUserName= await userCollectionObject.findOne({Name: userBody.Name});
-    if(resultOfUserName!=null){
-        let result= await bcryptjs.compare(userBody.CurrentPassword,resultOfUserName.Password);
-        if(result==false){
-            response.send({message:"Invalid password"})
-        }
-        else{
-        let Newpassword=userBody.NewPassword;
-        let hashedPassword=await bcryptjs.hash(Newpassword,5);
-        let result1= await userCollectionObject.updateOne({Name: userBody.Name},{$set:{Password:hashedPassword}});
-        response.send({message:'Password Changed'})
-    }
-
-    }
-    else{
-        response.send({message:'User not found. Please Signup'})
-    }
-    
-}))
 
 Userapp.delete('/deleteusers/:username',expressAsyncHandler(async(request,response)=>{
     let deleteUsername=request.params.username
