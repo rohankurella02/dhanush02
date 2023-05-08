f982dea88e5338701fb7aa0a5f479e0d26d2929b
rohankurella02 <rohankurella02@gmail.com>
2023-05-08 12:15:27 +0530

    BackNatural API

--- API/Score.js
+++ API/Score.js
@@ -4,6 +4,10 @@ const expressAsyncHandler=require('express-async-handler');
 Scoreapp.use(exp.json())
 // const verifyToken=require('./Middlewares/verifyToken')
 require('dotenv').config()
+const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
+const stopWords = new Set(natural.stopwords);
+const spacy = require('spacy');
 
 // Cloudinary
 // var cloudinary = require("cloudinary").v2;
@@ -34,6 +38,47 @@ require('dotenv').config()
 //   var upload = multer({ storage: cloudinaryStorage });
 
 Scoreapp.use(exp.urlencoded({ extended: true }));
+function summarizeText(text, summaryLength) {
+  // Clean the text
+  console.log("Inside function")
+  const tokens = tokenizer.tokenize(text);
+
+  // Remove stop words and punctuation
+  const cleanedTokens = tokens.filter(token => {
+    return !stopWords.has(token.toLowerCase()) && token.match(/\w+/);
+  });
+
+  const cleanedText = cleanedTokens.join(' ');
+
+  // Tokenize the cleaned text into sentences
+  const sentenceTokenizer = new natural.SentenceTokenizer();
+  const sentences = sentenceTokenizer.tokenize(cleanedText);
+
+  // Create a TF-IDF vector space model for the sentences
+  const tfidf = new natural.TfIdf();
+  sentences.forEach(sentence => {
+    tfidf.addDocument(sentence);
+  });
+
+  // Calculate the TextRank score for each sentence
+  const scores = {};
+  for (let i = 0; i < sentences.length; i++) {
+    let score = 0;
+    tfidf.listTerms(i).forEach(term => {
+      score += term.tfidf;
+    });
+    scores[sentences[i]] = score;
+  }
+
+  // Sort the sentences by TextRank score and select the top ones
+  const summarySentences = Object.keys(scores)
+    .sort((a, b) => scores[b] - scores[a])
+    .slice(0, summaryLength);
+
+
+  // Join the summary sentences into a string and return
+  return summarySentences.join(' ');
+}
 
 Scoreapp.get('/get-Score/:username',expressAsyncHandler(async(request,response)=>{
   let userName=request.params.username
@@ -57,6 +102,14 @@ Scoreapp.post('/create-Score',expressAsyncHandler(async(request,response)=>{
 
 }))
 
+Scoreapp.get('/summarize/:Text', expressAsyncHandler(async (request, response) => {
+  let Text = request.params.Text
+  // const summary = summarizeText(Text);
+  const summary = summarizeText(Text, 2);
+  // console.log(Text); // Outputs "This is some example text."
+  response.send({ message: 'Score Added', text: Text, summaryText: summary })
+
+}))
 // Favoriteapp.delete('/delete-fav/:name/:place',expressAsyncHandler(async(request,response)=>{
 //     let deletename=request.params.name;
 //     let deleteplace=request.params.place;
